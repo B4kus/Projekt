e599f590a4f954008b8e7fe2b0d563edc7f1b626
Szymon Szysz <sszysz@netizens.pl>
2018-01-14 13:12:28 +0100

    Add new view controller file

---
+++ MyFavouriteDog/MyFavouriteDog/FavouriteViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FavouriteViewController.swift
+//  MyFavouriteDog
+//
+//  Created by Szymon Szysz on 14.01.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+class FavouriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
