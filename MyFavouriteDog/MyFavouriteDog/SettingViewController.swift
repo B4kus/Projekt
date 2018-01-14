cc2cdaeef45ed833f022037c0aa188b114e808d9
Szymon Szysz <sszysz@netizens.pl>
2018-01-14 13:53:08 +0100

    Add new file Setting View Controller

---
+++ MyFavouriteDog/MyFavouriteDog/SettingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingViewController.swift
+//  MyFavouriteDog
+//
+//  Created by Szymon Szysz on 14.01.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    //MARK: - View Section
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
